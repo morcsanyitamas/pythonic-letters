a63596b251637f20652ea94a244ff14d08c20625
gyongyiovics <gyongyimako@gmail.com>
2020-06-11 14:57:04 +0200

    my version

---
+++ pythonic_letters_MGyongyi.py
@@ -0,0 +1,8 @@
+word = "Pythonic letters"
+def print_letters(word):
+    pythonic_word_list = []
+    for letter in word:
+        pythonic_word_list.append(letter)    
+    return pythonic_word_list
+
+        
